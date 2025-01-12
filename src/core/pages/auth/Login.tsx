6a4c118a02fae7f2643c5a8bd303248a08670eeb
Gilbert Blankson-Afful <101939909+ekowamoonu@users.noreply.github.com>
2025-01-12 09:02:33 +0000

    Added login functionality

--- src/core/pages/auth/Login.tsx
+++ src/core/pages/auth/Login.tsx
@@ -6,50 +6,138 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { IErrorResponse } from "@/core/interfaces/IErrorResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
 
 export function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true);
+    try {
+      const response = await axios.post("/login", values);
+      localStorage.setItem("auth_token", response.data.token);
+      navigate("/overview");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        (error.response?.data as IErrorResponse).message.forEach((message) => {
+          toast(message, { type: "error" });
+        });
+      } else if (error instanceof Error) {
+        console.log(error.message);
+      } else {
+        console.log(error);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <div className="flex flex-col gap-6 justify-center items-center min-h-svh">
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl">Login to your account</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
+    <>
+      <ToastContainer />
+      <div className="flex flex-col gap-6 justify-center items-center min-h-svh">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-2xl">Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-2">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              id="email"
+                              type="email"
+                              placeholder="m@example.com"
+                              required
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    ></FormField>
+                  </div>
+                  <div className="grid gap-2">
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem className="space-y-1">
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input
+                              id="password"
+                              type="password"
+                              required
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    ></FormField>
+                  </div>
+                  {isLoading ? (
+                    <Button disabled className="w-full">
+                      <Loader2 className="animate-spin" />
+                      Login
+                    </Button>
+                  ) : (
+                    <Button type="submit" className="w-full">
+                      Login
+                    </Button>
+                  )}
                 </div>
-                <Input id="password" type="password" required />
-              </div>
-              <Button type="submit" className="w-full">
-                Login
-              </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="/signup" className="underline underline-offset-4">
-                Sign up
-              </a>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+                <div className="mt-4 text-center text-sm">
+                  Don&apos;t have an account?{" "}
+                  <a href="/signup" className="underline underline-offset-4">
+                    Sign up
+                  </a>
+                </div>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 }
--- src/core/pages/auth/Signup.tsx
+++ src/core/pages/auth/Signup.tsx
@@ -15,82 +15,146 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { IErrorResponse } from "@/core/interfaces/IErrorResponse";
+import { useNavigate } from "react-router-dom";
+
+const formSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  password: z.string(),
+});
+
+// interface SignupResponse {
+//   data: { token: string };
+// }
 
 const SignupForm = () => {
-  const form = useForm();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true);
+    try {
+      const response = await axios.post("/signup", values);
+      localStorage.setItem("auth_token", response.data.token);
+      navigate("/overview");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        (error.response?.data as IErrorResponse).message.forEach((message) => {
+          toast(message, { type: "error" });
+        });
+      } else if (error instanceof Error) {
+        console.log(error.message);
+      } else {
+        console.log(error);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
-    <div className="flex flex-col gap-6 justify-center items-center min-h-svh">
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl">Signup</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <div className="flex flex-col gap-3">
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem className="space-y-1">
-                      <FormLabel>Name</FormLabel>
-                      <FormControl>
-                        <Input
-                          className="mt-[100px]"
-                          placeholder="Gilbert Blankson-Afful"
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
+    <>
+      <ToastContainer />
+      <div className="flex flex-col gap-6 justify-center items-center min-h-svh">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-2xl">Signup</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-3">
+                  <div className="grid gap-2">
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem className="space-y-1">
+                          <FormLabel>Name</FormLabel>
+                          <FormControl>
+                            <Input
+                              className="mt-[100px]"
+                              placeholder="Gilbert Blankson-Afful"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem className="space-y-1 mb-2">
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              type="email"
+                              placeholder="m@example.com"
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem className="space-y-1 mb-2">
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input {...field} type="password" />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  {isLoading ? (
+                    <Button disabled className="w-full">
+                      <Loader2 className="animate-spin" />
+                      Create my free account
+                    </Button>
+                  ) : (
+                    <Button type="submit" className="w-full">
+                      Create my free account
+                    </Button>
                   )}
-                />
-              </div>
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem className="space-y-1 mb-2">
-                      <FormLabel htmlFor="email">Email</FormLabel>
-                      <Input
-                        {...field}
-                        type="email"
-                        placeholder="m@example.com"
-                      />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem className="space-y-1 mb-2">
-                      <FormLabel>Password</FormLabel>
-                      <Input {...field} type="password" />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <Button type="submit" className="w-full">
-                Create my free account
-              </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Already have an account?{" "}
-              <a href="/login" className="underline underline-offset-4">
-                Login here
-              </a>
-            </div>
-          </Form>
-        </CardContent>
-      </Card>
-    </div>
+                </div>
+                <div className="mt-4 text-center text-sm">
+                  Already have an account?{" "}
+                  <a href="/login" className="underline underline-offset-4">
+                    Login here
+                  </a>
+                </div>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 };
 
